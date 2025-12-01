04ba67a77966d8c61a6ec1fbac2fbbf8124c3e5a
Gloria-fr <yang.lixj@icloud.com>
2025-12-01 23:47:16 +0100

    AppRouter

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -1,19 +1,22 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Error from './pages/Error'
+// src/App.jsx
+
+import React from 'react' // 只要用了JSX，引入React总是没错的
+import Header from './components/Header'
+import AppRouter from './components/AppRouter' // <--- 引入刚才那个单独的组件
+
+// 注意：这里不需要再引入 Routes, Route, Home, About 了，因为都在 AppRouter 里
+
 import './App.css'
 
 function App() {
   return (
-    <div>
-      {/* Routes 是路由容器，Route 是具体的规则 */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        {/* path="*" 意思是“匹配所有”，用来做 404 页面 */}
-        <Route path="*" element={<Error />} />
-      </Routes>
+    <div className="App">
+      <Header />
+      
+      {/* 这里不再写一长串 Route，而是直接放一个组件 */}
+      <AppRouter /> 
+      
+      {/* 以后这里还会放 <Footer /> */}
     </div>
   )
 }
