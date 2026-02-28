d6fd480ab7b1c3a214376a35486aa0790c6f39f5
Gloria-fr <yang.lixj@icloud.com>
2026-02-28 16:40:37 +0100

    media

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import AppRouter from './components/AppRouter'
-
+import './styles/Index.scss';
 
 function App() {
   return (
